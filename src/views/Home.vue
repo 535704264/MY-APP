f9b68a62c8253adb17eb058e175adcc528047e0c
nidazhong <nidazhong17@163.com>
2022-12-29 23:38:28 +0800

    update header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,31 @@
             <p>上次登陆地点：<span>广州</span></p>
           </div>
         </el-card>
+        <el-card style="margin-top: 20px;height: 350px;">
+          <el-table
+              :data="tableData"
+              stripe
+              style="width: 100%">
+            <el-table-column v-for="(val, key) in tableLabel" :prop="key" :label="val"></el-table-column>
+
+<!--            <el-table-column-->
+<!--                prop="name"-->
+<!--                label="课程">-->
+<!--            </el-table-column>-->
+<!--            <el-table-column-->
+<!--                prop="todayBuy"-->
+<!--                label="今日购买">-->
+<!--            </el-table-column>-->
+<!--            <el-table-column-->
+<!--                prop="monthBuy"-->
+<!--                label="本月购买">-->
+<!--            </el-table-column>-->
+<!--            <el-table-column-->
+<!--                prop="totalBuy"-->
+<!--                label="总购买">-->
+<!--            </el-table-column>-->
+          </el-table>
+        </el-card>
       </el-col>
       <el-col :span="16"><div class="grid-content bg-purple-light"></div></el-col>
     </el-row>
@@ -22,7 +47,52 @@
 <script>
 export default {
   data() {
-    return{}
+    return{
+      tableData: [
+        {
+          name: 'oppo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'vivo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '苹果',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '小米',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '三星',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '魅族',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        }
+      ],
+      tableLabel: {
+        name: '课程',
+        todayBuy: '今日购买',
+        monthBuy:'本月购买',
+        totalBuy: '总购买'
+      }
+    }
   }
 }
 </script>
