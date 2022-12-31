dce19ca72e86cca0f85e6205c06e0f2816dfc3de
nidazhong <nidazhong17@163.com>
2022-12-31 16:03:38 +0800

    update login

--- src/router/index.js
+++ src/router/index.js
@@ -20,13 +20,13 @@ const routes = [
         component: Main, // 根路径页面
         name: 'Main',
         redirect: '/home',
-        children: [
-            // {path: 'home', name: 'home', component: Home}, // 首页
-            // {path: 'user', name: 'user', component: User}, // 用户管理
-            // {path: 'mall',  name: 'mall',component: Mall},  // 商品管理
-            // {path: 'page1', name: 'page1',component: PageOne}, // 页面1
-            // {   path: 'page2', name: 'page2', component: PageTwo},  // 页面2
-        ]
+        // children: [
+        //     {path: 'home', name: 'home', component: Home}, // 首页
+        //     {path: 'user', name: 'user', component: User}, // 用户管理
+        //     {path: 'mall',  name: 'mall',component: Mall},  // 商品管理
+        //     {path: 'page1', name: 'page1',component: PageOne}, // 页面1
+        //     {   path: 'page2', name: 'page2', component: PageTwo},  // 页面2
+        // ]
     },
     {
         path: "/login",
